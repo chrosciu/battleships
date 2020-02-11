925ca891fee204de2354b53db4ace972b79662ca
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-02-11 23:29:04 +0100

    shifting calculations moved to Direction enum

--- src/main/java/com/chrosciu/Direction.java
+++ src/main/java/com/chrosciu/Direction.java
@@ -1,13 +1,18 @@
 package com.chrosciu;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+import java.util.function.BiFunction;
+
+@RequiredArgsConstructor
 @Getter
 public enum Direction {
-    VERTICAL,
-    HORIZONTAL;
+    VERTICAL((field, shift) -> new Field(field.getX(), field.getY() + shift)),
+    HORIZONTAL((field, shift) -> new Field(field.getX() + shift, field.getY()));
 
     private Direction perpendicular;
+    private final BiFunction<Field, Integer, Field> shiftField;
 
     static {
         VERTICAL.perpendicular = HORIZONTAL;
