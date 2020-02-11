3907100655f28f1bba5bb69bec396c9b0cf26708
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-02-11 22:50:04 +0100

    refactored main game class

---
+++ src/main/java/com/chrosciu/BattleshipsGame.java
@@ -0,0 +1,54 @@
+package com.chrosciu;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+import static com.chrosciu.Result.FINISHED;
+
+public class BattleshipsGame {
+
+    private static final List<Integer> SHIP_SIZES = Collections.singletonList(1);
+    private static final int BOARD_SIZE = 2;
+
+    private final BoardShooter boardShooter;
+    private final Scanner keyboard;
+
+    public static void main(String[] args) {
+        new BattleshipsGame().playGame();
+    }
+
+    public BattleshipsGame() {
+        List<Ship> ships = new BoardLocator(BOARD_SIZE).locateShipsWithGivenSizeOnBoard(SHIP_SIZES);
+        this.boardShooter = new BoardShooter(ships);
+        this.keyboard = new Scanner(System.in);
+    }
+
+    public void playGame() {
+        while (true) {
+            boolean gameFinished = handleUserMoveAndCheckIfFinished();
+            if (gameFinished) {
+                break;
+            }
+        }
+    }
+
+    private boolean handleUserMoveAndCheckIfFinished() {
+        Field field = readFieldFromUserInput(keyboard);
+        Result result = boardShooter.takeShot(field);
+        handleShotResult(result);
+        return FINISHED == result;
+    }
+
+    private Field readFieldFromUserInput(Scanner keyboard) {
+        System.out.println("Enter field vertical coordinate (x)");
+        int x = keyboard.nextInt();
+        System.out.println("Enter field horizontal coordinate (y)");
+        int y = keyboard.nextInt();
+        return new Field(x, y);
+    }
+
+    private void handleShotResult(Result result) {
+        System.out.println(result);
+    }
+}
